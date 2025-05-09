59296500102298fabe6b1851ae913e2fc86bddba
solidchrysalis <musicemail7171@gmail.com>
2025-05-08 23:36:10 -0400

    working towards multiplication

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate tfhe;
 
-use tfhe::{prelude::*, FheUint32};
-use tfhe::{generate_keys, set_server_key, ConfigBuilder};
+use tfhe::prelude::*;
+use tfhe::{generate_keys, set_server_key, ConfigBuilder, FheUint32Array};
 
 fn main() {
     let config = ConfigBuilder::default().build();
@@ -11,10 +11,40 @@ fn main() {
 
     set_server_key(server_key);
 
-    let clear_xs = (0..3 as u32).collect::<Vec<_>>();
-    let clear_ys = vec![2u32; 3];
+    let num_elems = 4 * 4;
+    let clear_xs = (0..num_elems as u32).collect::<Vec<_>>();
+    let clear_ys = vec![1u32; num_elems];
 
-    // Encrypted 1D array with values
+    // Encrypted 2D array with values
+    // [[  0,  1,  2,  3]
+    //  [  4,  5,  6,  7]
+    //  [  8,  9, 10, 11]
+    //  [ 12, 13, 14, 15]]
+    let xs = FheUint32Array::try_encrypt((clear_xs.as_slice(), vec![4, 4]), &client_key).unwrap();
+    // Encrypted 2D array with values
+    // [[  1,  1,  1,  1]
+    //  [  1,  1,  1,  1]
+    //  [  1,  1,  1,  1]
+    //  [  1,  1,  1,  1]]
+    let ys = FheUint32Array::try_encrypt((clear_ys.as_slice(), vec![4, 4]), &client_key).unwrap();
+
+    let x_dim = xs.shape();
+    let y_dim = ys.shape();
+    //Only working for 2d arrays rn
+    assert_eq!(xs.num_dim(), 2);
+    assert_eq!(ys.num_dim(), 2);
+    assert_eq!(x_dim.get(1), y_dim.get(0));
+
+    //let test = xs.get_slice(&[1..2, 1..2]).unwrap();
+    //let sum = ys.get_slice(&[1..2, 1..2]).unwrap();
+    //let r = &test * &sum;
+    //let result: Vec<u32> = r.decrypt(&client_key);
+    //assert_eq!(result, vec![5]);
+}
+
+// Code without slicing
+/* 
+    // Encrypted 1D array wdith values
     // [0, 1, 2]
     //let xs = FheUint32Array::try_encrypt((clear_xs.as_slice(), vec![3, 1]), &client_key).unwrap();
     let xxs: Vec<FheUint32> = clear_xs.iter().map(|&x| FheUint32::encrypt(x, &client_key)).collect();
@@ -38,5 +68,4 @@ fn main() {
     let help: u32 = FheUint32::decrypt(&sum, &client_key);
 
     assert_eq!(help, 6);
-}
-
+    */
